34c24ca449c1015db3c453767448e10efa4a71a8
AlchyRotmg <njgaudon@gmail.com>
2025-06-02 10:57:15 -0230

    small fix

--- keyScripts.js
+++ keyScripts.js
@@ -324,15 +324,15 @@ function startMemeMode() {
   document.body.appendChild(memeContainer);
 
   const memes = [
-    '/images/image1.png',
-    '/images/image2.png',
-    '/images/image3.png',
-    '/images/image4.png',
-    '/images/image5.png',
-    '/images/image6.png',
-    '/images/image7.png',
-    '/images/image8.png',
-    '/images/image9.png',
+    'images/image1.png',
+    'images/image2.png',
+    'images/image3.png',
+    'images/image4.png',
+    'images/image5.png',
+    'images/image6.png',
+    'images/image7.png',
+    'images/image8.png',
+    'images/image9.png',
     '/images/image10.png',
   ];
 
